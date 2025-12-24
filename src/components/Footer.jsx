e13b7ef750e876a6c71fafba8ddc5466d4c33da1
shekgit <shekhar214569@gmail.com>
2025-12-24 23:21:15 +0530

    Footer link updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <div
         className={"text-center pb-3 capitalize bg-black w-full"}
         >
-            <a href="https://github.com/shekgit/fashion-portal-using-react">Github Link</a>
+            <a href="https://github.com/shekgit/fashion-portal-using-react/tree/main">Github Link</a>
         </div>
     );
 };
